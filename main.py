37f49a3f0c755c067c5d1ee40628347210107adb
Bassel Attia <bassel_sabour@hotmail.com>
2023-04-20 16:46:59 +0200

    remove unnecessary files

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from handler import TalkingHeads, RequestLimitExceeded
 from config import *
 
 try:
-    two_heads = TalkingHeads(USERNAME, PASSWORD, headless=False)
+    two_heads = TalkingHeads(USERNAME, PASSWORD, headless=True)
     r1,r2 = two_heads.start_conversation('From now on, you are a superhero who is trying to convince me (a villain) not to destroy the world. Keep your answers short.' , 'From now on, you are a villain who wants to take over the world, and you will be talking to me, a superhero who is trying to convince you that this is bad. Keep your answers short. Here is how it starts:')
     num_replies = 1
     for i in range(num_replies+1):
