ab15e17e74855a2509108ab4922c38a932dddbac
Ugur Sahin <ugur.sahin@tum.de>
2024-03-16 17:55:03 +0100

    feat(is_url_check_filetype): new functions to check filetype and if the string is url

--- src/talkingheads/utils.py
+++ src/talkingheads/utils.py
@@ -2,9 +2,28 @@
 import re
 import subprocess
 import logging
-from typing import Union
+from typing import List, Union
 
+import filetype
 from undetected_chromedriver import find_chrome_executable
+import validators
+
+
+def check_filetype(filepath, extensions: List[str]) -> bool:
+    """Checks if the given file is expected, return False if the extension
+    is not included in expected extensions
+
+    Args:
+        filepath (str): the path to file
+        extensions (List[str]): accepted file extensions
+
+    Returns:
+        bool: True if file extension exists in extension list, False otherwise
+    """
+
+    extension = filetype.guess_extension(filepath)
+
+    return extension in extensions
 
 
 def detect_chrome_version(version_num: int = None) -> Union[int, None]:
@@ -40,6 +59,18 @@ def detect_chrome_version(version_num: int = None) -> Union[int, None]:
     version_num = int(out.group(1))
     logging.info("The version is %d", version_num)
     return version_num
+
+
+def is_url(possible_url: str) -> bool:
+    """Checks if the given string is a valid url
+
+    Args:
+        possible_url (str): A string, possibly a url
+
+    Returns:
+        bool: True if possible_url is a valid url, false otherwise 
+    """
+    return validators.url(possible_url)
 
 
 save_func_map = {
