3ffe87e23567997fdec934ca9ead4532965f59d6
Ugur Sahin <ugur.sahin@tum.de>
2025-02-09 23:05:25 +0100

    refactor(LeChat): Replacing selenium with nodriver, transforming everything to async.

--- src/talkingheads/model_library/lechat.py
+++ src/talkingheads/model_library/lechat.py
@@ -1,6 +1,8 @@
 """Class definition for LeChat client"""
 import time
+import os
 
+import nodriver as nd
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from ..base_browser import BaseBrowser
@@ -20,7 +22,7 @@ class LeChatClient(BaseBrowser):
             **kwargs,
         )
 
-    def login(self, username: str, password: str):
+    async def login(self):
         """
         Performs the login process with the provided username and password.
 
@@ -37,47 +39,25 @@ class LeChatClient(BaseBrowser):
         """
 
         # Find username input area, enter e-mail
-        email_box = self.wait_until_appear(By.XPATH, self.markers.username_xq)
-        email_box.send_keys(username)
+        # Find email textbox, enter e-mail
+        email_box = await self.wait_until_appear(self.markers.username)
+        await email_box.send_keys(os.environ.get(self.uname_var))
         self.logger.info("Filled username/email")
 
-        # Find password input area, enter password
-        pass_box = self.wait_until_appear(By.XPATH, self.markers.password_xq)
-        pass_box.send_keys(password)
+        # Find password textbox, enter password
+        pass_box = await self.wait_until_appear(self.markers.password)
+        await pass_box.send_keys(os.environ.get(self.pwd_var))
         self.logger.info("Filled password box")
 
-        pass_box.send_keys(Keys.ENTER)
+        verification_box = await self.wait_until_appear(self.markers.verify)
+        if verification_box:
+            verification_box.click()
+
+        await pass_box.send_keys('\r\n')
 
         return True
 
-    def get_last_response(self, tick_time : int = 200, tick_period : float = 0.5) -> str:
-        """Retrieves the last response given by ChatGPT
-
-        Returns:
-            str: The last response
-        """
-        self.logger.info("Checking the response")
-        self.wait_until_appear(By.XPATH, self.markers.chatbox_xq)
-        # self.wait_until_disappear(By.XPATH, self.markers.stop_gen_xq)
-
-        for _ in range(tick_time):
-            time.sleep(tick_period)
-            l_response = self.find_or_fail(
-                By.XPATH, self.markers.chatbox_xq, return_type="last"
-            ).text
-            if l_response and l_response == self.interim_response:
-                break
-            self.interim_response = l_response
-
-        if not self.interim_response:
-            self.logger.error("There is no response, something is wrong")
-            return ""
-
-        self.logger.info("response is ready")
-        return self.interim_response
-
-
-    def interact(self, prompt: str):
+    async def interact(self, prompt: str):
         """Sends a prompt and retrieves the response.
 
         This function interacts with the LeChat.
@@ -94,69 +74,39 @@ class LeChatClient(BaseBrowser):
             str: The generated response.
         """
 
-        text_area = self.find_or_fail(By.XPATH, self.markers.textarea_xq)
+        text_area = await self.find_or_fail(self.markers.textarea)
         if not text_area:
             return ""
 
         for each_line in prompt.split("\n"):
-            text_area.send_keys(each_line)
-            text_area.send_keys(Keys.SHIFT + Keys.ENTER)
-        text_area.send_keys(Keys.RETURN)
+            await text_area.send_keys(each_line)
+            await text_area.send_keys('\r\n')
+
+        send_button = await self.find_or_fail(self.markers.send)
+        await send_button.click()
+
         self.logger.info("Message sent, waiting for response")
         self.last_prompt = prompt
-        response = self.get_last_response()
+        response = await self.get_last_response()
         if not response:
             return ""
         self.logger.info("response is ready")
         self.log_chat(prompt=prompt, response=response)
         return response
 
-    def reset_thread(self):
+    async def reset_thread(self):
         """Function to close the current thread and start new one"""
-        self.browser.get(self.url)
+        self.tab = await self.browser.get(self.url)
         return True
 
-    def switch_model(self, model_name: str):
-        """
-        Switch the model.
-
-        Args:
-            model_name: str = The name of the model
-
-        Returns:
-            bool: True on success, False on fail
-        """
-        model_button = self.find_or_fail(By.XPATH, self.markers.model_xq)
-        if not model_button:
-            return False
-        model_button.click()
-
-        models = self.find_or_fail(
-            By.XPATH, self.markers.model_op_xq, return_type="all"
-        )
-        if not models:
-            return False
-        models = {m.text.split("\n")[0]: m for m in models}
-
-        model = models.get(model_name, None)
-        if model is None:
-            self.logger.error("Model %s has not found", model_name)
-            self.logger.error("Available models are: %s", str(models.keys()))
-            model_button.click()
-            return False
-
-        model.click()
-        self.logger.info("Switched to %s", model_name)
-        return True
-
-    def regenerate_response(self):
-        regen_button = self.find_or_fail(By.XPATH, self.markers.regen_xq)
+    async def regenerate_response(self):
+        regen_button = await self.find_or_fail(self.markers.regen)
         if not regen_button:
             return ""
-        regen_button.click()
+        await regen_button.click()
         self.logger.info("Clicked regenerate button")
 
-        response = self.get_last_response()
+        response = await self.get_last_response()
         if not response:
             return ""
         self.log_chat(response=response, regenerated=True)
