a553abe800e94ee6df9f459930e8db09e9fabf2c
Ugur Sahin <ugur.sahin@tum.de>
2025-02-09 23:07:33 +0100

    test: Generic new test_reset, refactoring to async

--- tests/generic.py
+++ tests/generic.py
@@ -1,18 +1,24 @@
 import pytest
-import time
 import psutil
 
-def test_interaction():
-    response = pytest.chathead.interact(
+async def test_interaction(chathead):
+    response = await chathead.interact(
         "What object is most often found on a bookshelf?"
     )
     assert (
         "book" in response.lower()
     ), f'response is not "book.", the full response: {response}'
-    time.sleep(0.5)
 
-def test_delete_chathead():
-    del pytest.chathead
+async def test_delete_chathead(chathead):
+    chathead.browser.stop()
+    del chathead
     assert not any(
-        "undetected_chromedriver" in p.name() for p in psutil.process_iter()
-    ), "Undetected chromedriver exists"
+        "nodriver" in p.name() for p in psutil.process_iter()
+    ), "nodriver exists"
+
+async def test_reset(chathead):
+    assert await chathead.reset_thread()
+    chat_item = await chathead.find_or_fail(chathead.markers.chatbox, fail_ok=True)
+    assert (
+        chat_item is None
+    ), "Chat is not empty"
