6a02c3652ebbe8ce24d1612f56ce94b5f24a1709
Ugur Sahin <ugur.sahin@tum.de>
2025-02-09 23:03:42 +0100

    refactor(Copilot): Replacing selenium with nodriver, transforming everything to async.

--- src/talkingheads/model_library/copilot.py
+++ src/talkingheads/model_library/copilot.py
@@ -1,13 +1,11 @@
 """Class definition for Copilot client"""
 
+import asyncio
 import time
 from pathlib import Path
 from typing import Union
 
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
+from nodriver import cdp
 from ..base_browser import BaseBrowser
 from ..utils import check_filetype, is_url
 
@@ -27,7 +25,7 @@ class CopilotClient(BaseBrowser):
             **kwargs,
         )
 
-    def login(self, username: str = None, password: str = None):
+    async def login(self):
         """
         Performs the login process with the provided username and password.
         You don't need to login to use Pi
@@ -46,13 +44,13 @@ class CopilotClient(BaseBrowser):
         self.logger.info("Login is not provided for Copilot at the moment.")
         return True
 
-    def postload_custom_func(self) -> None:
+    async def postload_custom_func(self) -> None:
         """Copilot requires to accept privacy terms, the cookie below provides the response."""
-        self.browser.add_cookie({"name": "BCP", "value": "AD=0&AL=0&SM=0"})
-        self.browser.get(self.url)
+        # self.browser.add_cookie({"name": "BCP", "value": "AD=0&AL=0&SM=0"})
+        await self.browser.get(self.url)
         time.sleep(1)
 
-    def is_ready_to_prompt(self) -> bool:
+    async def is_ready_to_prompt(self) -> bool:
         """
         Checks if the Copilot is ready to be prompted.
         The indication for an ongoing message generation process
@@ -64,20 +62,17 @@ class CopilotClient(BaseBrowser):
         Returns:
             bool : return if the system is ready to be prompted.
         """
-        text_area = self.find_or_fail(By.XPATH, self.markers.textarea_xq)
-        text_area.send_keys(".")
-
-        submit_button = self.find_or_fail(By.XPATH, self.markers.submit_xq)
-        if not submit_button:
-            return False
-
-        self.wait_object.until(EC.element_to_be_clickable(submit_button))
+        text_area = await self.find_or_fail(self.markers.textarea)
+        await text_area.send_keys(".")
+        await self.wait_until_appear(self.markers.disabled_send)
+        await self.wait_until_disappear(self.markers.disabled_send)
 
         # Then, we clear the text area to make space for new interacton :)
-        text_area.send_keys(Keys.CONTROL + "a", Keys.DELETE)
+        await self.clear_textarea()
+
         return True
 
-    def get_last_response(self) -> str:
+    async def get_last_response(self) -> str:
         """Returns the last response in the chat view.
 
         Args:
@@ -92,13 +87,15 @@ class CopilotClient(BaseBrowser):
         # failing the last check below. If check_greeting is set,
         # we can return the text of this greeting element, instead of parsing the response.
 
-        resp = self.find_or_fail(By.XPATH, self.markers.answer_xq, return_type="last")
+        await self.wait_until_appear(self.markers.chatbox)
+
+        resp = await self.find_or_fail(self.markers.chatbox, return_type="last")
         if not resp:
             return ""
 
-        return resp.text
+        return resp.text_all
 
-    def upload_image(self, image_path: Union[str, Path]) -> bool:
+    async def upload_image(self, image_path: Union[str, Path]) -> bool:
         """Upload an image or a url and wait until it is uploaded,
         then returns.
 
@@ -118,28 +115,27 @@ class CopilotClient(BaseBrowser):
             self.logger.warning("Given path is neither an image path nor a url")
             return False
 
-        im_input_element = self.find_or_fail(By.XPATH, self.markers.img_upload_xq)
-        im_input_element.send_keys(image_path)
+        im_input_element = await self.find_or_fail(self.markers.upload_img)
+        await im_input_element.send_file(image_path)
 
-        condition = EC.element_to_be_clickable((By.XPATH, self.markers.dismiss_xq))
-        WebDriverWait(self.browser, 10).until(condition)
-        return True
+        discard_button = await self.wait_until_appear(self.markers.dismiss, fail_ok=True)
+        return discard_button is not None
 
-    def remove_attached_image(self) -> bool:
+    async def remove_attached_image(self) -> bool:
         """Removes the attached image from prompt
 
         Returns:
             bool: True if the action is valid
         """
-        discard_button = self.find_or_fail(By.XPATH, self.markers.dismiss_xq)
+        discard_button = await self.find_or_fail(self.markers.dismiss)
         if discard_button:
             self.logger.info("Clicking remove image button")
-            discard_button.click()
+            await discard_button.click()
             return True
 
         return False
 
-    def interact(self, prompt: str, image_path: Union[str, Path] = None) -> str:
+    async def interact(self, prompt: str, image_path: Union[str, Path] = None) -> str:
         """Sends a prompt and retrieves the response from the Copilot system.
 
         This function interacts with the Copilot.
@@ -157,35 +153,46 @@ class CopilotClient(BaseBrowser):
             Dict[str]: The generated response.
         """
 
-        text_area = self.find_or_fail(By.XPATH, self.markers.textarea_xq)
+        text_area = await self.find_or_fail(self.markers.textarea)
 
         if not text_area:
             self.logger.error("Unable to locate text area, interaction fails.")
             return ""
         if image_path:
-            self.upload_image(image_path)
+            uploaded = await self.upload_image(image_path)
+            if not uploaded:
+                self.logger.error('Image upload failed.')
+                return ""
 
-        for each_line in prompt.split("\n"):
-            text_area.send_keys(each_line)
-            text_area.send_keys(Keys.SHIFT + Keys.ENTER)
+        prompt = prompt.replace("\n", "\r\n")
+        await text_area.focus()
+        await self.tab.send(cdp.input_.insert_text(text=prompt))
 
-        # Click enter and send the message
-        text_area.send_keys(Keys.ENTER)
+        send_button = await self.find_or_fail(self.markers.send)
+        await send_button.click()
 
-        self.close_location_modal()
-        if image_path:
-            time.sleep(1)
-
-        if not self.is_ready_to_prompt():
+        if not await self.is_ready_to_prompt():
             self.logger.info("Cannot retrieve the response, something is wrong")
             return ""
 
-        text = self.get_last_response()
+        text = await self.get_last_response()
         self.logger.info("response is ready")
         self.log_chat(prompt=prompt, response=text)
         return text
 
-    def reset_thread(self) -> bool:
+    async def clear_textarea(self, wait_duration=0.3):
+        """Waits a little and clears the input. By waiting, it avoids
+        any inconvenience caused by rendering
+
+        Args:
+            wait_duration (float, optional): the waiting time before acting. Defaults to 0.3.
+        """
+        time.sleep(wait_duration)
+        text_area = await self.find_or_fail(self.markers.textarea)
+        await BaseBrowser.clear_input(text_area)
+        return
+
+    async def reset_thread(self) -> bool:
         """
         Function to close the current thread and start new one
 
@@ -194,30 +201,32 @@ class CopilotClient(BaseBrowser):
         """
 
         # Find home button, but it may be fine even the button is not there.
-        home_button = self.find_or_fail(By.XPATH, self.markers.home_xq)
-        if not home_button:
-            self.logger.info("Home button is not found, trying view history button")
-        else:
-            home_button.click()
-
-        # Find view history button to find new chat button.
-        view_history_button = self.find_or_fail(By.XPATH, self.markers.history_xq)
-        if not view_history_button:
+        open_menu_button = await self.find_or_fail(self.markers.open)
+        if not open_menu_button:
             return False
-        view_history_button.click()
+
+        await open_menu_button.mouse_click()
 
         # Find new chat button.
-        new_chat_button = self.find_or_fail(By.XPATH, self.markers.new_chat_xq)
+        new_chat_button = await self.wait_until_appear(self.markers.new_chat)
         if not new_chat_button:
             return False
-        new_chat_button.click()
+        await new_chat_button.mouse_click()
+        await self.clear_textarea()
+        await asyncio.sleep(0.3)
 
         return True
 
-    def regenerate_response(self):
+    async def regenerate_response(self):
         raise NotImplementedError("Copilot doesn't provide response regeneration")
 
-    def close_location_modal(self):
-        maybe_later = self.find_or_fail(By.XPATH, self.markers.location_xq, fail_ok=True)
-        if maybe_later:
-            maybe_later.click()
+    async def toggle_think_deeper(self):
+        search_button = await self.find_or_fail(self.markers.think)
+        await search_button.click()
+        search_button = await self.find_or_fail(self.markers.think_active)
+        return search_button is not None
+
+    # async def close_location_modal(self):
+    #     maybe_later = await self.find_or_fail(self.markers.location, fail_ok=True)
+    #     if maybe_later:
+    #         maybe_later.click()
