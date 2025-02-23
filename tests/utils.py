82ed611ee256bd6f7e045a5630bfc099d520f9ea
Ugur Sahin <ugur.sahin@tum.de>
2025-02-23 22:25:44 +0100

    test: Remove unnecessary parameters, update incognito param

--- tests/utils.py
+++ tests/utils.py
@@ -4,12 +4,12 @@ import os
 from typing import Any, Dict
 from datetime import datetime
 
-def get_driver_arguments(name: str, incognito: bool = False) -> Dict[str, Any]:
+def get_driver_arguments(name: str, no_user_data_dir: bool = False) -> Dict[str, Any]:
     """Returns the parameters to start client
 
     Args:
         name (str): name of the client
-        incognito (bool, optional): Defaults to True.
+        no_user_data_dir (bool, optional): Sets user data dir to None. Defaults to True.
 
     Returns:
         Dict[str, Any]: Arguments to the client
@@ -20,14 +20,5 @@ def get_driver_arguments(name: str, incognito: bool = False) -> Dict[str, Any]:
         "verbose": True,
         "auto_save": True,
         "save_path": f"artifacts/{name}_{datetime.now().strftime('%Y_%m_%d_%H_%M_%S.csv')}",
-        "driver_arguments": [
-            "--disable-gpu",
-            "--disable-dev-shm-usage",
-            "--enable-logging",
-            "--v=1",
-            f"--log-file=artifacts/{name}_chrome.log",
-            "--password-store=basic"
-        ],
-        "incognito": incognito,
-        "user_data_dir": os.getenv('CHROME_USER_DATA_DIR')
+        "user_data_dir": os.getenv('CHROME_USER_DATA_DIR') if not no_user_data_dir else None
     }
