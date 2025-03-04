4cb3a14456fba1c7b64368bfd90aeab4c960905b
Ugur Sahin <ugur.sahin@tum.de>
2025-03-04 13:30:44 +0100

    refactor: remove undetected chrome code, add nodriver to dependencies, add dev dependencies.

--- src/talkingheads/model_library/lechat.py
+++ src/talkingheads/model_library/lechat.py
@@ -1,10 +1,6 @@
 """Class definition for LeChat client"""
-import time
 import os
 
-import nodriver as nd
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from ..base_browser import BaseBrowser
 
 
--- src/talkingheads/utils.py
+++ src/talkingheads/utils.py
@@ -2,10 +2,9 @@
 import re
 import subprocess
 import logging
-from typing import List, Union
+from typing import List, Union, Optional
 
 import filetype
-from undetected_chromedriver import find_chrome_executable
 import validators
 
 
@@ -23,44 +22,6 @@ def check_filetype(filepath, extensions: List[str]) -> Optional[str]:
 
     extension = filetype.guess_extension(filepath)
     return extension if extension in extensions else None
-
-
-def detect_chrome_version(version_num: int = None) -> Union[int, None]:
-    """
-    Detects the Google Chrome version on Linux and macOS machines.
-
-    Args
-        version_num (int, optional): The chromedriver version number. Default: None.
-
-    Returns:
-        int: The detected Google Chrome version number.
-
-    Note:
-    - If version_num is provided, it will be returned without any detection.
-
-    - Uses subprocess to execute the 'google-chrome --version' command for detection.
-
-    - If the command output doesn't match the expected format, it returns None.
-
-    - Logs information about the detected or default version using the logging module.
-    """
-
-    if version_num:
-        logging.debug("Version number is provided: %d", version_num)
-        return version_num
-
-    chrome_path = find_chrome_executable()
-
-    out = subprocess.check_output([chrome_path, "--version"])
-    out = re.search(r"Google\s+Chrome\s+(\d{3})", out.decode())
-
-    if not out:
-        logging.error("There was an error obtaining Chrome version")
-        return None
-
-    version_num = int(out.group(1))
-    logging.info("The version is %d", version_num)
-    return version_num
 
 
 def is_url(possible_url: str) -> bool:
